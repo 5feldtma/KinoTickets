b17ae649e8e3f7c8707406e93ec8849c53a413c2
LennSE2 <LennSE2@users.noreply.github.com>
2016-06-13 02:34:59 +0200

    Zusatz: Mehr Filme

--- src/de/uni_hamburg/informatik/swt/se2/kino/startup/StartupKinoticketverkauf_Blatt07.java
+++ src/de/uni_hamburg/informatik/swt/se2/kino/startup/StartupKinoticketverkauf_Blatt07.java
@@ -1,5 +1,9 @@
 package de.uni_hamburg.informatik.swt.se2.kino.startup;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 import javax.swing.SwingUtilities;
 
 import de.uni_hamburg.informatik.swt.se2.kino.fachwerte.Datum;
@@ -19,6 +23,7 @@ import de.uni_hamburg.informatik.swt.se2.kino.werkzeuge.kasse.KassenWerkzeug;
  */
 public class StartupKinoticketverkauf_Blatt07
 {
+	
     /**
      * Die Main-Methode.
      * 
@@ -41,17 +46,45 @@ public class StartupKinoticketverkauf_Blatt07
      */
     private static Kino erzeugeKinoMitBeispieldaten()
     {
+    	Film[] filme = new Film[256];
+    	
         final Kinosaal[] saele = { new Kinosaal("Saal 1", 20, 25),
                 new Kinosaal("Saal 2", 16, 20), new Kinosaal("Saal 3", 10, 16) };
 
         // Filme: Top-5 Deutschland laut kino.de in der Kalenderwoche 20, 2011.
-        Film[] filme = {
-                new Film("Pirates of the Caribbean - Fremde Gezeiten", 136,
-                        FSK.FSK12, true),
-                new Film("Fast & Furious Five", 130, FSK.FSK12, true),
-                new Film("Rio", 96, FSK.FSK0, false),
-                new Film("Wasser für die Elefanten", 120, FSK.FSK12, false),
-                new Film("Thor", 115, FSK.FSK12, false) };
+
+                filme[0] = new Film("Pirates of the Caribbean - Fremde Gezeiten", 136,
+                        FSK.FSK12, true);
+                filme[1] = new Film("Fast & Furious Five", 130, FSK.FSK12, true);
+                filme[2] = new Film("Rio", 96, FSK.FSK0, false);
+                filme[3] = new Film("Wasser für die Elefanten", 120, FSK.FSK12, false);
+                filme[4] = new Film("Thor", 115, FSK.FSK12, false);       
+
+        
+        int index = 5;
+        
+     // TODO filme
+    	Scanner sc;
+    	File movies = new File("movies/movies.txt");
+
+    	
+		try {
+			sc = new Scanner(movies, "ISO-8859-1");
+			String filmtitel;
+	    	System.out.println(sc.hasNext());
+	        while (sc.hasNextLine()) {
+				filmtitel = sc.nextLine();
+				int runtime = (int) Math.floor(Math.random() * (90)) + 60;
+				filme[index] = new Film(filmtitel, runtime, FSK.FSK18, runtime > 120);
+				++index;
+			}
+	        sc.close();
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+    	
+        
+        System.out.println(filme[8]);
 
         Uhrzeit nachmittag = new Uhrzeit(17, 30);
         Uhrzeit abend = new Uhrzeit(20, 0);
@@ -93,6 +126,9 @@ public class StartupKinoticketverkauf_Blatt07
 
                 new Vorstellung(saele[2], filme[3], abend, spaet, d3, 1000),
                 new Vorstellung(saele[2], filme[4], spaet, nacht, d3, 900) };
+        
+        
+       
 
         return new Kino(saele, vorstellungen);
     }
