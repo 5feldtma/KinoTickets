50d174001782afcfb5924fdcae10e2871ee3097d
LennSE2 <LennSE2@users.noreply.github.com>
2016-06-13 12:29:17 +0200

    Mehr Daten haben Filme. Hurra!

--- src/de/uni_hamburg/informatik/swt/se2/kino/startup/StartupKinoticketverkauf_Blatt07.java
+++ src/de/uni_hamburg/informatik/swt/se2/kino/startup/StartupKinoticketverkauf_Blatt07.java
@@ -21,30 +21,27 @@ import de.uni_hamburg.informatik.swt.se2.kino.werkzeuge.kasse.KassenWerkzeug;
  * @author SE2-Team
  * @version SoSe 2016
  */
-public class StartupKinoticketverkauf_Blatt07
-{
-	
-    /**
-     * Die Main-Methode.
-     * 
-     * @param args die Aufrufparameter.
-     */
-    public static void main(String[] args)
-    {
-        final Kino kino = erzeugeKinoMitBeispieldaten();
-        SwingUtilities.invokeLater(new Runnable()
-        {
-            public void run()
-            {
-                new KassenWerkzeug(kino);
-            }
-        });
-    }
+public class StartupKinoticketverkauf_Blatt07 {
 
-    /**
-     * Erzeugt ein Kino mit einigen Vorstellungen.
-     */
-    private static Kino erzeugeKinoMitBeispieldaten()
+	/**
+	 * Die Main-Methode.
+	 * 
+	 * @param args
+	 *            die Aufrufparameter.
+	 */
+	public static void main(String[] args) {
+		final Kino kino = erzeugeKinoMitBeispieldaten();
+		SwingUtilities.invokeLater(new Runnable() {
+			public void run() {
+				new KassenWerkzeug(kino);
+			}
+		});
+	}
+
+	/**
+	 * Erzeugt ein Kino mit einigen Vorstellungen.
+	 */
+	private static Kino erzeugeKinoMitBeispieldaten()
     {
     	Film[] filme = new Film[256];
     	
@@ -71,7 +68,6 @@ public class StartupKinoticketverkauf_Blatt07
 		try {
 			sc = new Scanner(movies, "ISO-8859-1");
 			String filmtitel;
-	    	System.out.println(sc.hasNext());
 	        while (sc.hasNextLine()) {
 				filmtitel = sc.nextLine();
 				int runtime = (int) Math.floor(Math.random() * (90)) + 60;
@@ -82,51 +78,73 @@ public class StartupKinoticketverkauf_Blatt07
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
 		}
-    	
-        
-        System.out.println(filme[8]);
+		
+		
 
         Uhrzeit nachmittag = new Uhrzeit(17, 30);
         Uhrzeit abend = new Uhrzeit(20, 0);
         Uhrzeit spaet = new Uhrzeit(22, 30);
         Uhrzeit nacht = new Uhrzeit(1, 0);
 
+        Uhrzeit[] uhrzeiten = {nachmittag, abend, spaet, nacht};
+        
         Datum d1 = Datum.heute();
         Datum d2 = d1.naechsterTag();
         Datum d3 = d2.naechsterTag();
+        Datum datum = d3.naechsterTag();
 
-        final Vorstellung[] vorstellungen = {
+        Vorstellung[] vorstellungen = new Vorstellung[187];
+        
                 // Heute
-                new Vorstellung(saele[0], filme[2], nachmittag, abend, d1, 500),
-                new Vorstellung(saele[0], filme[0], abend, spaet, d1, 700),
-                new Vorstellung(saele[0], filme[0], spaet, nacht, d1, 700),
+                vorstellungen[0] = new Vorstellung(saele[0], filme[2], nachmittag, abend, d1, 500);
+                vorstellungen[1] = new Vorstellung(saele[0], filme[0], abend, spaet, d1, 700);
+                vorstellungen[2] = new Vorstellung(saele[0], filme[0], spaet, nacht, d1, 700);
 
-                new Vorstellung(saele[1], filme[3], nachmittag, abend, d1, 900),
-                new Vorstellung(saele[1], filme[1], spaet, nacht, d1, 800),
+                vorstellungen[3] = new Vorstellung(saele[1], filme[3], nachmittag, abend, d1, 900);
+                vorstellungen[4] = new Vorstellung(saele[1], filme[1], spaet, nacht, d1, 800);
 
-                new Vorstellung(saele[2], filme[3], abend, spaet, d1, 1000),
-                new Vorstellung(saele[2], filme[4], spaet, nacht, d1, 900),
+                vorstellungen[5] = new Vorstellung(saele[2], filme[3], abend, spaet, d1, 1000);
+                vorstellungen[6] = new Vorstellung(saele[2], filme[4], spaet, nacht, d1, 900);
 
                 // Morgen
-                new Vorstellung(saele[0], filme[0], abend, spaet, d2, 500),
-                new Vorstellung(saele[0], filme[0], spaet, nacht, d2, 700),
+                vorstellungen[7] = new Vorstellung(saele[0], filme[0], abend, spaet, d2, 500);
+                vorstellungen[8] = new Vorstellung(saele[0], filme[0], spaet, nacht, d2, 700);
 
-                new Vorstellung(saele[1], filme[2], nachmittag, abend, d2, 900),
-                new Vorstellung(saele[1], filme[4], abend, nacht, d2, 800),
+                vorstellungen[9] = new Vorstellung(saele[1], filme[2], nachmittag, abend, d2, 900);
+                vorstellungen[10] = new Vorstellung(saele[1], filme[4], abend, nacht, d2, 800);
 
-                new Vorstellung(saele[2], filme[3], nachmittag, abend, d2, 1000),
-                new Vorstellung(saele[2], filme[1], spaet, nacht, d2, 900),
+                vorstellungen[11] = new Vorstellung(saele[2], filme[3], nachmittag, abend, d2, 1000);
+                vorstellungen[12] = new Vorstellung(saele[2], filme[1], spaet, nacht, d2, 900);
 
                 // Übermorgen
-                new Vorstellung(saele[0], filme[1], abend, spaet, d3, 500),
-                new Vorstellung(saele[0], filme[1], spaet, nacht, d3, 700),
+                vorstellungen[13] = new Vorstellung(saele[0], filme[1], abend, spaet, d3, 500);
+                vorstellungen[14] = new Vorstellung(saele[0], filme[1], spaet, nacht, d3, 700);
 
-                new Vorstellung(saele[1], filme[2], nachmittag, abend, d3, 900),
-                new Vorstellung(saele[1], filme[0], abend, nacht, d3, 800),
+                vorstellungen[15] = new Vorstellung(saele[1], filme[2], nachmittag, abend, d3, 900);
+                vorstellungen[16] = new Vorstellung(saele[1], filme[0], abend, nacht, d3, 800);
 
-                new Vorstellung(saele[2], filme[3], abend, spaet, d3, 1000),
-                new Vorstellung(saele[2], filme[4], spaet, nacht, d3, 900) };
-        
+                vorstellungen[17] = new Vorstellung(saele[2], filme[3], abend, spaet, d3, 1000);
+                vorstellungen[18] = new Vorstellung(saele[2], filme[4], spaet, nacht, d3, 900);
+                 
+                for (int i = 19; i < vorstellungen.length; i++) {
+                	
+                	
+                	
+                	if (i % 6 == 0)
+                	{
+                		datum = datum.naechsterTag();
+                	}
+
+                	
+                	int saalnummer = 0 + (int)(Math.random() * ((2) + 1));
+                	int filmnummer = 0 + (int)(Math.random() * ((252) + 1));
+                	int preis =  500 + (int)(Math.random() * ((500) + 1));
+                	Film film = filme[filmnummer];
+                	int zeit = 0 + (int)(Math.random() * ((2) + 1));
+                	Uhrzeit startzeit = uhrzeiten[zeit];
+                	Uhrzeit ende = uhrzeiten[zeit +1];
+					vorstellungen[i] = new Vorstellung(saele[saalnummer], film, startzeit, ende, datum, preis);
+				}
         
        
 
